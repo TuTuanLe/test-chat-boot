ca8f5798f2479194ee8319af795d9d9282ce210c
TuTuanLe <tutuanletkpt@gmail.com>
2022-09-26 22:54:05 +0700

    fix: verify token

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const BootBot = require('BootBot');
 
 const bot = new BootBot({
   accessToken:'EAARKjqIsmgsBAEZB85ENWjlph9FZBwP5ZBAFDHDazgTd8B4lQgPpslxQXy8ZBnTw4yUnGBRYZAkp2bbAivZC5mUKZAbqqCeYRBbSkc7gUSyhQlUwBAh27ab8Vpq6sIpDrjqsaBnurlQM0wwyFSwayzSzQ2hDJUP1jZBzvanRFZCgZAmI34wlaUwfZA4',
-  verifyToken:'',
+  verifyToken:'tutuanletkpt',
   appSecret:'b8b71b6f5f0bca8c2df902a29a450466'  
 })
 
+bot.hear(['hello', 'hi', 'hey'], (payload, chat) =>{
+    chat.say('hello there');
+});
+
+bot.start();

08d40430c61fc96be4b8cc88241071f5079da225
TuTuanLe <tutuanletkpt@gmail.com>
2022-09-26 22:22:54 +0700

    test chatboot

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+const BootBot = require('BootBot');
+
+const bot = new BootBot({
+  accessToken:'EAARKjqIsmgsBAEZB85ENWjlph9FZBwP5ZBAFDHDazgTd8B4lQgPpslxQXy8ZBnTw4yUnGBRYZAkp2bbAivZC5mUKZAbqqCeYRBbSkc7gUSyhQlUwBAh27ab8Vpq6sIpDrjqsaBnurlQM0wwyFSwayzSzQ2hDJUP1jZBzvanRFZCgZAmI34wlaUwfZA4',
+  verifyToken:'',
+  appSecret:'b8b71b6f5f0bca8c2df902a29a450466'  
+})
+

f8ee4d46ea4ca0c38382517bcebeb474fbc0d76e
TuTuanLe <tutuanletkpt@gmail.com>
2022-09-26 23:32:12 +0700

    add package look :

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const BootBot = require('BootBot');
 
 const bot = new BootBot({
   accessToken:'EAARKjqIsmgsBAEZB85ENWjlph9FZBwP5ZBAFDHDazgTd8B4lQgPpslxQXy8ZBnTw4yUnGBRYZAkp2bbAivZC5mUKZAbqqCeYRBbSkc7gUSyhQlUwBAh27ab8Vpq6sIpDrjqsaBnurlQM0wwyFSwayzSzQ2hDJUP1jZBzvanRFZCgZAmI34wlaUwfZA4',
-  verifyToken:'tutuanletkpt',
+  verifyToken:'tutuanle',
   appSecret:'b8b71b6f5f0bca8c2df902a29a450466'  
 })
 
